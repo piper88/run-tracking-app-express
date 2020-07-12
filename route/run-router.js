4aff05bcb877280a2d5f1c07828b98b31d8a678f
Sarah DeBey <sarah.debey@gmail.com>
2020-07-12 11:39:14 -0700

    adds one passing test for getting valid run

--- route/run-router.js
+++ route/run-router.js
@@ -1,13 +1,24 @@
 //exports a function that takes req, res, and next, and calls next at end
 
 const express = require('express');
-const router = express.Router();
+const Router = express.Router;
+const url = require('url');
+const querystring = require('querystring');
+
+const runRouter = new Router();
 
 const storage = require('../lib/storage.js');
 
-module.exports = exports = router;
+module.exports = exports = runRouter;
 
-router.get('/api/run', function (req, res, next) {
+var parseUrl = function(req, res, next) {
+  req.url = url.parse(req.url);
+  req.url.query = querystring.parse(req.url.query);
+  next();
+};
+
+
+runRouter.get('/api/run', parseUrl, function (req, res) {
   storage.fetchItem(req.url.query.date)
     .then(run => {
       res.writeHead(200, {'Content-Type': 'application/json'});
---
+++ test/run-router-test.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const request = require('superagent');
+const expect = require('chai').expect;
+const storage = require('../lib/storage.js');
+
+require('../server.js');
+
+describe('testing run routes', function() {
+  storage.createResourceDirectory();
+
+  describe('testing GET /api/run', function() {
+    describe('with valid run date', function () {
+      before(done => {
+        let run = {
+          date: 'today',
+          distance: 2,
+          pace: 700,
+        };
+        storage.createItem(run)
+        .then(() => done())
+        .catch((err) => done(err));
+      });
+      after(done => {
+        storage.deleteItem('today')
+        .then(() => done())
+        .catch((err) => done(err));
+      });
+      it('should return a run', function(done) {
+        request.get('localhost:3000/api/run?date=today')
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.distance).to.equal(2);
+          done();
+        });
+      });
+    });
+    // describe('with missing run date', function() {
+    //   it('should return a 404 not found error', function(done) {
+    //     request.get('localhost:3000/api/run')
+    //     .end((err, res) => {
+    //       expect(res.status).to.equal(404);
+    //       done();
+    //     });
+    //   });
+    // });
+
+    // describe('with invalid run date', function() {
+    //   it('should return a 404 not found error', function(done) {
+    //     request.get('localhost:3000/api/run?date=neverp')
+    //     .end((err, res) => {
+    //       expect(res.status).to.equal(404);
+    //       done();
+    //     });
+    //   });
+    // });
+  });
+});
