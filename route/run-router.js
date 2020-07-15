6728911c228f350ad68ece0d99a4b7054e77589a
Sarah DeBey <sarah.debey@gmail.com>
2020-07-15 12:01:22 -0700

    adds error middleware with one passing test for missing date in get request

--- route/run-router.js
+++ route/run-router.js
@@ -1,9 +1,7 @@
 //exports a function that takes req, res, and next, and calls next at end
 
-const express = require('express');
-const Router = express.Router;
-// const url = require('url');
-// const querystring = require('querystring');
+const Router = require('express').Router;
+const debug = require('debug')('run:run-router');
 
 const runRouter = new Router();
 
@@ -12,17 +10,24 @@ const storage = require('../lib/storage.js');
 module.exports = exports = runRouter;
 
 
-runRouter.get('/api/run/:date', function (req, res) {
+runRouter.get('/api/run', function (req, res, next) {
+  debug('route GET /api/run');
+  // var err = new Error('please work');
+  // next(err);
   storage.fetchItem(req.params.date)
     .then(run => {
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      res.end(run);
+      res.json(run);
+      // res.writeHead(200, {'Content-Type': 'application/json'});
+      // res.end(run);
     })
     .catch(err => {
+      console.log('yo');
+      //err will either be expected date, in which case, no error status, or....??????
+      next(err);
       //error from rejected promise
-      console.error(err);
-      //error sent to client
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end();
+      // console.error(err);
+      // //error sent to client
+      // res.writeHead(404, {'Content-Type': 'text/plain'});
+      // res.end();
     });
 });
--- test/run-router-test.js
+++ test/run-router-test.js
@@ -3,6 +3,7 @@
 const request = require('superagent');
 const expect = require('chai').expect;
 const storage = require('../lib/storage.js');
+const debug = require('debug')('run:run-test');
 
 require('../server.js');
 
@@ -10,50 +11,18 @@ describe('testing run routes', function() {
   storage.createResourceDirectory();
 
   describe('testing GET /api/run', function() {
-    describe('with valid run date', function () {
-      before(done => {
-        let run = {
-          date: 'today',
-          distance: 2,
-          pace: 700,
-        };
-        storage.createItem(run)
-        .then(() => done())
-        .catch((err) => done(err));
-      });
-      after(done => {
-        storage.deleteItem('today')
-        .then(() => done())
-        .catch((err) => done(err));
-      });
-      it('should return a run', function(done) {
-        request.get('localhost:3000/api/run/today')
-        .end((err, res) => {
-          if (err) return done(err);
-          expect(res.status).to.equal(200);
-          expect(res.body.distance).to.equal(2);
-          done();
-        });
-      });
-    });
+
     describe('with missing run date', function() {
-      it('should return a 404 not found error', function(done) {
-        request.get('localhost:3000/api/run')
+      debug('testing with missing run date');
+      it('should return a 400 bad request', function(done) {
+        request.get('localhost:3000/api/run/')
         .end((err, res) => {
-          expect(res.status).to.equal(404);
+          expect(res.status).to.equal(400);
+          // expect(err).to.equal('expected date');
           done();
         });
       });
     });
 
-    describe('with invalid run date', function() {
-      it('should return a 404 not found error', function(done) {
-        request.get('localhost:3000/api/run?date=neverp')
-        .end((err, res) => {
-          expect(res.status).to.equal(404);
-          done();
-        });
-      });
-    });
   });
 });
