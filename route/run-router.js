0af326c4b508eac385b235e9a015145db3a9f19b
Sarah DeBey <sarah.debey@gmail.com>
2020-07-15 15:54:57 -0700

    adds second test for getting valid run

--- route/run-router.js
+++ route/run-router.js
@@ -10,7 +10,7 @@ const storage = require('../lib/storage.js');
 module.exports = exports = runRouter;
 
 
-runRouter.get('/api/run', function (req, res, next) {
+runRouter.get('/api/run/:date', function (req, res, next) {
   debug('route GET /api/run');
   // var err = new Error('please work');
   // next(err);
@@ -21,7 +21,6 @@ runRouter.get('/api/run', function (req, res, next) {
       // res.end(run);
     })
     .catch(err => {
-      console.log('yo');
       //err will either be expected date, in which case, no error status, or....??????
       next(err);
       //error from rejected promise
--- test/run-router-test.js
+++ test/run-router-test.js
@@ -8,17 +8,44 @@ const debug = require('debug')('run:run-test');
 require('../server.js');
 
 describe('testing run routes', function() {
-  storage.createResourceDirectory();
+  // storage.createResourceDirectory();
+  let run = {
+    date: 'today',
+    distance: 2,
+    pace: 700,
+  };
 
   describe('testing GET /api/run', function() {
 
+    describe('with valid date', function() {
+      debug('testing with valid date');
+      before(done => {
+      storage.createItem(run)
+      .then(() => done())
+      .catch((err) => done(err));
+    })
+    after(done => {
+      storage.deleteItem(run.date)
+      .then(() => done())
+      .catch((err) => done(err));
+    })
+      it('should return a run', function(done) {
+        request.get(`localhost:3000/api/run/today`)
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.status).to.equal(200);
+          done()
+        })
+      })
+    })
+
     describe('with missing run date', function() {
       debug('testing with missing run date');
-      it('should return a 400 bad request', function(done) {
-        request.get('localhost:3000/api/run/')
+      it('should return a 404 bad request', function(done) {
+        request.get('localhost:3000/api/run')
         .end((err, res) => {
-          expect(res.status).to.equal(400);
-          // expect(err).to.equal('expected date');
+          expect(res.status).to.equal(404);
+          // expect(res.error.text).to.equal('"expected date"');
           done();
         });
       });
