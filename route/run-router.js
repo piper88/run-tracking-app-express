0d77631c7063e8d420769c91024ff7c90e3b61a5
Sarah DeBey <sarah.debey@gmail.com>
2020-07-18 16:01:10 -0700

    adds horrendously nested promised put route and adds tests thereof

--- route/run-router.js
+++ route/run-router.js
@@ -43,7 +43,7 @@ runRouter.post('/api/run', parseJSON, function (req, res, next) {
 runRouter.delete('/api/run/:date', function (req, res, next) {
   debug('route DELETE /api/run/:date');
   return storage.deleteItem(req.params.date)
-  .then((date) => {
+  .then(() => {
     debug('then after storage.deleteItem');
     res.status(204);
     res.send('successfully deleted run');
@@ -51,5 +51,39 @@ runRouter.delete('/api/run/:date', function (req, res, next) {
   .catch(err => {
     next(err);
   })
-
 })
+
+runRouter.put('/api/run/:date', parseJSON, function(req, res, next) {
+  //this is horrendous
+  debug('route PUT /api/run/:date');
+  return storage.doesItemExist(req.params.date)
+  .then(() => {
+    //if it exists, then try to create new one
+    let run = new Run(req.body.date, req.body.distance, req.body.pace)
+    .then((theRun) => {
+      return storage.deleteItem(req.params.date)
+      .then(() => {
+        return storage.createItem(theRun)
+        .then(newRun => {
+          res.json(newRun);
+        })
+        .catch(err => {
+          //failed to create
+          next(err);
+        })
+      })
+      .catch(err => {
+        //failed to delete
+        next(err);
+      })
+    })
+    .catch(err => {
+      //missing required property of run (failed in new Run)
+      next(err);
+    })
+  })
+  //run not found
+  .catch(err => {
+    next(err);
+  })
+});
