d564a5e24c07de888a7ea73b4f8d555776f926c3
Sarah DeBey <sarah.debey@gmail.com>
2020-07-17 17:38:23 -0700

    adds body-parser to parse post requests, adds route for post requests and one passing test for valid body

--- model/run.js
+++ model/run.js
@@ -1,8 +1,13 @@
+const createError = require('http-errors');
+
 
 module.exports = exports = function (date, distance, pace) {
-  if (!date) throw new Error('expected date');
-  if (!distance) throw new Error('expected distance');
-  if (!pace) throw new Error('expected pace');
+  // if (!date) throw new Error('expected date');
+  if (!date) return Promise.reject(createError(400, 'expected date'));
+  // if (!distance) throw new Error('expected distance');
+  if (!distance) return Promise.reject(createError(400, 'expected distance'));
+  // if (!pace) throw new Error('expected pace');
+  if (!pace) return Promise.reject(createError(400, 'expected pace'));
   this.date = date;
   this.distance = distance;
   this.pace = pace;
--- route/run-router.js
+++ route/run-router.js
@@ -2,16 +2,18 @@
 
 const Router = require('express').Router;
 const debug = require('debug')('run:run-router');
+const parseJSON = require('body-parser').json();
+const Run = require('../model/run.js');
 
 const runRouter = new Router();
 
 const storage = require('../lib/storage.js');
 
-module.exports = exports = runRouter;
+module.exports = runRouter;
 
 
 runRouter.get('/api/run/:date', function (req, res, next) {
-  debug('route GET /api/run');
+  debug('route GET /api/run/:date');
   storage.fetchItem(req.params.date)
     .then(run => {
       res.json(run);
@@ -19,4 +21,20 @@ runRouter.get('/api/run/:date', function (req, res, next) {
     .catch(err => {
       next(err);
     });
+});
+
+runRouter.post('/api/run', parseJSON, function (req, res, next) {
+  debug('route POST /api/run');
+  try {
+    let run = new Run(req.body.date, req.body.distance, req.body.pace)
+    storage.createItem(run)
+    .then(run => {
+      res.json(run);
+    })
+    .catch(err => {
+      next(err);
+    });
+  } catch (err) {
+      next(err);
+  };
 });
