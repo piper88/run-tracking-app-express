3983ce0cf3cb422c8cbace3813d4ceb75999f62d
Sarah DeBey <sarah.debey@gmail.com>
2020-08-20 11:10:35 -0700

    formats date of requests and stores in database in format MMMM Do, YYYY

--- model/run.js
+++ model/run.js
@@ -3,12 +3,15 @@ const debug = require('debug');
 let mongoose = require('mongoose');
 let validator = require('validator');
 
+
+
 var runSchema = new mongoose.Schema({
+  //user should input date as YYYY/MM/DD
+  //manipulate it to be Month Date, Year //e.g. June 1, 2020
   date: {type: String, required: [true, 'Date required']},
   distance: {type: Number, required: [true, 'Distance required']},
   pace: {type: Number, required: [true, 'Pace required']}
 })
-
 
 //mongoose validates before saving, thus pre hook isn't running with post example with missing body, because it's failing on validation, doesn't even get to saving.
 // runSchema.pre('save', function() {
@@ -17,27 +20,10 @@ var runSchema = new mongoose.Schema({
 // })
 
 //mongoose recognizes this as error handler, because of 3 paramaters including error. Anytime an erorr is triggered in pre hook, or save, this runs.
-runSchema.post('save', function(error, doc, next) {
-  console.log('post.save error handler');
-  // console.error(error.errors);
-  // console.error(error.name); //ValidationError
-  // console.log(error.errors.date.message) //this gets us the custom error message set in the Schema (e.g. Date required)
-  // console.error(error.errors.date.properties);
-  //this next passes error back to rejected promise in runRouter
-  next(error);
-})
+// runSchema.post('save', function(error, doc, next) {
+//   //this next passes error back to rejected promise in runRouter
+//   next(error);
+// })
 
 //be sure to define any pre/post hooks before compiling the model (the line below). Otherwise the hooks won't be added.
 module.exports = mongoose.model('Run', runSchema);
-
-//WITH MISSING BODY, pre hook not running, post hook is
-//OUTPUT:
-// run:run-router route POST /api/run +0ms
-// post save hook
-//   run:run-router catch??????? +10ms
-//   run:error-handling error handling middleware +0ms
-
-//WITH VALID BODY, pre hook running, post hook not
-//OUTPUT
-// run:run-router route POST /api/run +0ms
-// pre save hook
--- route/run-router.js
+++ route/run-router.js
@@ -6,20 +6,20 @@ const parseJSON = require('body-parser').json();
 const RunModel = require('../model/run.js');
 const mongoose = require('mongoose');
 const createError = require('http-errors');
+const moment = require('moment');
 const runRouter = new Router();
 
-const storage = require('../lib/storage.js');
+
 
 module.exports = runRouter;
 
 runRouter.get('/api/run/:date', async function (req, res, next) {
   debug('route GET /api/run/:date');
-
-      //not finding a document specified in search will not throw an error.
+  req.params.date = moment(req.params.date).format('MMMM Do, YYYY');
+    //not finding a document specified in search will not throw an error.
     let run = await RunModel.findOne({date: req.params.date});
-
     if (run)  {
-      //rse.json identical to res.send when what's passed in is object or array, but res.json will also convert non objects(e.g. null and undefined)
+      //rse.json identical to res.send when what's passed in is object or array, but res.json will also convert non objects (e.g. null and undefined)
       res.json(run);
       return;
     }
@@ -31,7 +31,22 @@ runRouter.get('/api/run/:date', async function (req, res, next) {
 
 runRouter.post('/api/run', parseJSON, function (req, res, next) {
   debug('route POST /api/run');
-  let run = new RunModel({date: req.body.date, distance: req.body.distance, pace: req.body.pace});
+  let formattedDate;
+  let run;
+  switch (req.body.date) {
+    case 'today':
+      formattedDate = moment(Date.now()).format('MMMM Do, YYYY');
+      run = new RunModel({date: formattedDate, distance: req.body.distance, pace: req.body.pace});
+      break;
+    //if date missing from request
+    case undefined:
+      run = new RunModel({date: req.body.date, distance: req.body.distance, pace: req.body.pace});
+      break;
+    //if date in request is in a normal format
+    default:
+      formattedDate = moment(req.body.date).format('MMMM Do, YYYY');
+      run = new RunModel({date: formattedDate, distance: req.body.distance, pace: req.body.pace});
+  }
   run.save()
   .then(doc => {
     res.json(doc);
@@ -43,6 +58,11 @@ runRouter.post('/api/run', parseJSON, function (req, res, next) {
 
 runRouter.delete('/api/run/:date', function (req, res, next) {
   debug('route DELETE /api/run/:date');
+  if (req.params.date === 'today') {
+    req.params.date = moment(Date.now()).format('MMMM Do, YYYY');
+  } else {
+    req.params.date = moment(req.params.date).format('MMMM Do, YYYY');
+  }
   RunModel.deleteOne({date: req.params.date})
   .then((result) => {
     if (result.deletedCount == 0) {
@@ -57,18 +77,19 @@ runRouter.delete('/api/run/:date', function (req, res, next) {
   })
 })
 
-
-
-
-
 runRouter.put('/api/run/:date', parseJSON, async function(req, res, next) {
   debug('route PUT /api/run/:date');
-
   //checks for missing request body
   if (Object.keys(req.body).length == 0) {
     let err = createError(400, 'must update at least one field');
     next(err);
     return;
+  }
+  //changes date to format that exists in db
+  if (req.params.date === 'today') {
+    req.params.date = moment(Date.now()).format('MMMM Do, YYYY');
+  } else {
+    req.params.date = moment(req.params.date).format('MMMM Do, YYYY');
   }
   let filter = {date: req.params.date};
   let update = {date: req.body.date, distance: req.body.distance, pace: req.body.pace}
