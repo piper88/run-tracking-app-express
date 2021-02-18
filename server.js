80994e5160b97b818d974c06fcd3b1eb7b3b3043
Sarah DeBey <sarah.debey@gmail.com>
2021-02-17 17:07:28 -0800

    fixes date formats to fix failing tests

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.listen(PORT, () => {
   console.log(`server up on ${PORT}`);
 });
 
-
-mongoose.connect('mongodb://localhost:27017/run', {useNewUrlParser: true}).then(() => {
+//using atlas version of mongoDB
+mongoose.connect('mongodb+srv://sdebey88:8x18pi$74SNB@cluster0.wjb0y.mongodb.net/workouts?retryWrites=true&w=majority', {useFindAndModify: false, useNewUrlParser: true,  useUnifiedTopology: true}).then(() => {
   console.log('Database connection successful');
 }).catch(() => {
   console.error('Database connection unsuccessful');
@@ -25,3 +25,14 @@ mongoose.connect('mongodb://localhost:27017/run', {useNewUrlParser: true}).then(
 mongoose.connection.on('error', err => {
   console.error(err);
 });
+
+//to use locally downloaded version of mongoDB, change uri connection path to: mongodb://localhost:27017/<dbname>
+// mongoose.connect('mongodb://localhost:27017/run', {useFindAndModify: false, useNewUrlParser: true,  useUnifiedTopology: true}).then(() => {
+//   console.log('Database connection successful');
+// }).catch(() => {
+//   console.error('Database connection unsuccessful');
+// });
+//
+// mongoose.connection.on('error', err => {
+//   console.error(err);
+// });
