ccd9c678398e2c5e4cede4afacf5020dc00a1a23
Sarah DeBey <sarah.debey@gmail.com>
2021-03-05 10:17:40 -0800

    creates login route, compares passwords using bcrypt compare method

--- backend/lib/hash-pass.js
+++ backend/lib/hash-pass.js
@@ -7,6 +7,7 @@ const hashPass = (password) => {
   return new Promise((resolve, reject) => {
     //if password is missing, resolve null in order to handle error with mongoose validation
     if (!password) resolve(null);
+    //the 10 is the salt. Another layer of security. The hashed password is the hash plus the salt. Required so that the same password doesn't result in the same hash. So if two users have the same password, they won't have the same hashed password. If a hacker figures out one password then, they won't automatically have access to all user's hashed passwords that had the same plain text password.
     bcrypt.hash(password, 10, function(err, hashedPassword) {
       if (err) {
         return reject(createError(403, 'Invalid user credentials'));
--- backend/test/auth-router-test.js
+++ backend/test/auth-router-test.js
@@ -8,44 +8,61 @@ require('../server.js');
 
 describe('testing auth routes', function() {
   let user = {
-    email: 'piper88@gmail.com',
+    email: 'piper85@gmail.com',
     password: '123abc',
   };
 
-  describe('testing POST /api/run', function() {
+  describe('testing POST /api/signup', function() {
     describe('with valid user credentials', function() {
       it('should return a user', function(done) {
         request.post('localhost:3000/api/signup')
         .send(user)
         .end((err, res) => {
           if (err) return done(err);
-          expect(res.body.email).to.equal('piper88@gmail.com');
+          expect(res.body.email).to.equal('piper85@gmail.com');
           done();
         })
       });
     })
-    describe('with missing email', function() {
-      it('should return a 403 error', function(done) {
-        request.post('localhost:3000/api/signup')
+    // describe('with missing email', function() {
+    //   it('should return a 403 error', function(done) {
+    //     request.post('localhost:3000/api/signup')
+    //     .send({
+    //       password: 'passwort',
+    //     })
+    //     .end((err, res) => {
+    //       expect(res.body.message).to.equal('Email required\n');
+    //       expect(res.status).to.equal(403);
+    //       done();
+    //     })
+    //   })
+    // })
+    // describe('with missing password', function() {
+    //   it('should return a 403 error', function(done) {
+    //     request.post('localhost:3000/api/signup')
+    //     .send({
+    //       email: 'nick@nick.com',
+    //     })
+    //     .end((err, res) => {
+    //       expect(res.body.message).to.equal('Please enter password\n');
+    //       expect(res.status).to.equal(403);
+    //       done();
+    //     })
+    //   })
+    // })
+  })
+
+  describe('testing POST /api/login', function() {
+    describe('with valid user credentials', function() {
+      it('should return a token', function(done) {
+        request.post('localhost:3000/api/login')
         .send({
-          password: 'passwort',
+          email: 'piper85@gmail.com',
+          password: '123abc'
         })
         .end((err, res) => {
-          expect(res.body.message).to.equal('Email required\n');
-          expect(res.status).to.equal(403);
-          done();
-        })
-      })
-    })
-    describe('with missing password', function() {
-      it('should return a 403 error', function(done) {
-        request.post('localhost:3000/api/signup')
-        .send({
-          email: 'nick@nick.com',
-        })
-        .end((err, res) => {
-          expect(res.body.message).to.equal('Please enter password\n');
-          expect(res.status).to.equal(403);
+          if(err) return done(err);
+          expect(res.body.message).to.equal('User successfully logged in!');
           done();
         })
       })
